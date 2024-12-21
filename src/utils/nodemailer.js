b8d55c3f374a1e6288eba146c9ce1fbcfde30dcd
Jitesh Kumar <jiteshece@gmail.com>
2024-12-21 21:08:55 +0530

    nodemailer done

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -2,25 +2,18 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    // host: "smtp.ethereal.email",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
     auth: {
         user: "jiteshece@gmail.com",
         pass: process.env.GOOGLE_MAIL_PASS,
     },
 });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-    let message = {
-        from: '"Jitesh Kumar" <jiteshece@gmail.email>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    };
-
+// async..await is not allowed in global scope, i'm must use a wrapper
+async function main(message) {
     // send mail with defined transport object
     const info = await transporter.sendMail(message);
 
@@ -28,4 +21,14 @@ async function main() {
     // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
 }
 
-main().catch(console.error);
+let subject = "Hello From Node Mailer 2.0";
+let message = {
+    from: '"Jitesh Kumar" <jiteshece@gmail.email>', // sender address
+    to: "jitesbharti@gmail.com", // list of receivers
+    bcc: "vinishbhaskar321@gmail.com,soorajyadav137@gmail.com",
+    subject: subject, // Subject line
+    text: "Hello Bros, sup? !!!", // plain text body
+    html: "<b>Hello world?</b>", // html body
+};
+
+main(message).catch(console.error);
